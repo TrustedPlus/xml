6ed23b0193dd219f9d7263cf534b976e2af90363
Sergey Ivanov <iv.sergius@yandex.ru>
2016-11-25 18:05:26 +0300

    Module is modified common cryptography works in contrast with GOST

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-module.exports = require('./lib/signed-xml')
+//module.exports = require('./lib/signed-xml')
+module.exports = require('./lib/signed-xml-gost')
 module.exports.xpath = require('xpath.js')
--- test/signature-unit-tests-gost.js
+++ test/signature-unit-tests-gost.js
@@ -1,22 +1,22 @@
 var select = require('xpath.js')
-  , dom = require('xmldom').DOMParser
+  , DomParser = require('xmldom').DOMParser
   , SignedXml = require('../lib/signed-xml.js').SignedXml
   , FileKeyInfo = require('../lib/signed-xml.js').FileKeyInfo
   , xml_assert = require('./xml-assert.js')
   , fs = require('fs');
 
 module.exports = {
-
+  
   "signer creates correct signature values": function(test) {
-
+    debugger;
     var xml = "<root><x xmlns=\"ns\" Id=\"_0\"></x><y attr=\"value\" Id=\"_1\"></y><z><w Id=\"_2\"></w></z></root>";
     var sig = new SignedXml();
-    sig.signingKey = fs.readFileSync("./test/static/client.pem");
+    sig.signingKey = fs.readFileSync("./test/gost/private.pem");
     sig.keyInfoProvider = null;
 
-    sig.addReference("//*[local-name(.)='x']");
-    sig.addReference("//*[local-name(.)='y']");
-    sig.addReference("//*[local-name(.)='w']");
+    sig.addReference("//*[local-name(.)='x']", "http://www.w3.org/2001/10/xml-exc-c14n#", 'http://www.w3.org/2001/04/xmldsig-more#gostr3411');
+    sig.addReference("//*[local-name(.)='y']", "http://www.w3.org/2001/10/xml-exc-c14n#", 'http://www.w3.org/2001/04/xmldsig-more#gostr3411');
+    sig.addReference("//*[local-name(.)='w']", "http://www.w3.org/2001/10/xml-exc-c14n#", 'http://www.w3.org/2001/04/xmldsig-more#gostr3411');
 
     sig.computeSignature(xml);
     var signedXml = sig.getSignedXml();
@@ -28,29 +28,33 @@ module.exports = {
                     "<Reference URI=\"#_0\">" +
                     "<Transforms>" +
                     "<Transform Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/></Transforms>" +
-                    "<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmldsig-more#gostr34102001-gostr3411\"/>" +
+                    "<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmldsig-more#gostr3411\"/>" +
                     "<DigestValue>IL0BMorWqUsTIUOpcTKrf2dNYUbzgcO0X/pXr9jMQ9U=</DigestValue>" +
                     "</Reference>" +
                     "<Reference URI=\"#_1\">" +
                     "<Transforms>" +
                     "<Transform Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>" +
                     "</Transforms>" +
-                    "<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmldsig-more#gostr34102001-gostr3411\"/>" +
+                    "<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmldsig-more#gostr3411\"/>" +
                     "<DigestValue>L1MIC6+kkEnGKgtBGQPu4JpctCAmuJZ3aKSudVAsyuU=</DigestValue>" +
                     "</Reference>" +
                     "<Reference URI=\"#_2\">" +
                     "<Transforms>" +
                     "<Transform Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>" +
                     "</Transforms>" +
-                    "<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmldsig-more#gostr34102001-gostr3411\"/>" +
+                    "<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmldsig-more#gostr3411\"/>" +
                     "<DigestValue>hdvKiumKZUiDTEFrQBBogGQJjdTkYNCLvSlF9kVsDSM=</DigestValue>" +
                     "</Reference>" +
                     "</SignedInfo>" +
-                    "<SignatureValue>p7TwfF9dY4v5uq8MT4CdjA4MqOHoUkOL4rfEV4ocgwfcmuXRYBrpF1Og5q/wBLxaSw/3AdV7bxBHVBbmAC1pJg==</SignatureValue>" +
+                    "<SignatureValue></SignatureValue>" +
                     "</Signature>" +
                     "</root>";
 
-    test.equal(expected, signedXml, "wrong signature format");
+    var doc = new DomParser().parseFromString(signedXml);
+    // remove signature from SignatureValue element
+    doc.getElementsByTagName('SignatureValue')[0].childNodex[0].data = "";
+
+    test.equal(expected, doc.toString(), "wrong signature format");
 
     test.done();
   }
