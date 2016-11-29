07a3dfb99d929ed7bd9de8eff4c54b4d25d60364
Sergey Ivanov <iv.sergius@yandex.ru>
2016-11-29 14:11:45 +0300

    Add support of GOST 2001 sign
    Signed-off-by: Sergey Ivanov <iv.sergius@yandex.ru>

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-//module.exports = require('./lib/signed-xml')
 module.exports = require('./lib/signed-xml-gost')
+module.exports = require('./lib/signed-xml')
 module.exports.xpath = require('xpath.js')
